4dfb84d9c07a47af57e17e52bd1a4192dbb7d164
Isoraqathedh Zorethan <isoraqathedh.zorethan@gmail.com>
2017-03-18 16:49:28 +0000

    List the files that are required.

--- langdocgen.lisp
+++ langdocgen.lisp
@@ -2,5 +2,19 @@
 
 (in-package #:langdocgen)
 
-;;; "langdocgen" goes here. Hacks and glory await!
+(defvar *config-directory*
+  (merge-pathname
+   (make-pathname :directory '(:relative "bocproc"))
+   (uiop:xdg-config-home))
+  "Location of the directory that holds the config file.")
 
+(defvar *config-file*
+  (merge-pathname
+   (make-pathname :name "config" :type "lisp")
+   *config-directory*)
+  "Location of the file that holds the `bocproc' config file.")
+
+(defvar *data-file*
+  (merge-pathname
+   (make-pathname :name "data" :type "yaml")
+   *config-directory*))
